79441c406d1acfde05517ddeb00ee4ad752bd912
Ganesh-Gopal <mailgganesh@gmail.com>
2020-05-26 21:30:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-meer1y
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-meer1y)
---
+++ index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+import Phone from './Phone'
+
+class App extends Component {
+  constructor() {
+  }
+
+  render() {
+    return (
+      <h1> efsf</h1>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
