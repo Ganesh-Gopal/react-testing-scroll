42f9ae1922014e90d0881e66943d21011d01f735
Ganesh-Gopal <mailgganesh@gmail.com>
2020-05-27 06:19:00 +0530

    1 file modified

--- Phone/Phone.js
+++ Phone/Phone.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Parallax, Background } from 'react-parallax';
-import phone-mock-up from './phone-mock-up.png'
+import phone-mock-up from './phone-mock-up.png';
 
 class Phone extends Component {
   constructor() {
